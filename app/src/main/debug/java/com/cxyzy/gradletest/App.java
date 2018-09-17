5b761655d0d0649ceebf116f96f58bdcf7620a1a
xing.yin@ymm56.com <1qaz@WSX>
2018-09-17 10:23:04 +0800

    区分Debug和Release版本

---
+++ app/src/main/debug/java/com/cxyzy/gradletest/App.java
@@ -0,0 +1,21 @@
+package com.cxyzy.gradletest;
+
+import android.app.Application;
+
+public class App extends Application {
+    private static App instance;
+    private static final String TAG = App.class.getSimpleName();
+    public static App getInstance()
+    {
+        return instance;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+    public String getVersion()
+    {
+        return "debug";
+    }
+}
